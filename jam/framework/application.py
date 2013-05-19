a33440890e13fbe3fd38be278284348b785ee983
Justin Franchetto <justin.franchetto@live.ca>
2013-05-19 16:54:58 -0700

    Application is now a working class
    Changed jamtime.py to use some of the new methods, opens up a blue
    screen, but the exit commands no longer seem to work.

--- jam/framework/application.py
+++ jam/framework/application.py
@@ -11,35 +11,43 @@ from pygame.color import *
 from GameMode import *
 
 class Application:
-	
-	def __init__(self, gameMode):
-		self.gameMode = gameMode
-		
-	def setMode(self, newMode):
-		self.gameMode = newMode
-		pass
-	
-	def setupWindow(self):
-		width, height = 700, 400
-		
-		#Initialize Pygame
-		pygame.init()
-	
-		#Setup the window
-		pygame.display.set_caption('Jam Time') 
-		screen = pygame.display.set_mode((width, height))		
-	
-	def runGame(self):
-		running = True
-		
-		while running:
-		screen.fill(pygame.color.THECOLORS["blue"])
-		
-		for event in pygame.event.get():  
-			if event.type == QUIT or (event.type == KEYDOWN and (event.key in [K_ESCAPE, K_q])):        
-				running = False         
-			
-		pygame.display.flip()
+    def __init__(self, gameMode):
+        self.gameMode = gameMode
+        self.width = 700
+        self.height = 400
+        self.fps = 60
+        self.delta = 1/self.fps
+        self.clock = None
+        self.screen = None
+    
+    def init(self, gameMode):
+        pass
+        
+    def setMode(self, newMode):
+        self.gameMode = newMode
+    
+    def setupWindow(self):        
+        #Initialize Pygame
+        pygame.init()
+    
+        #Setup the window
+        pygame.display.set_caption('Jam Time') 
+        self.screen = pygame.display.set_mode((self.width, self.height))        
+        self.clock = pygame.time.Clock() 
+    
+    def runGame(self):
+        running = True
+        
+        while running:
+            self.screen.fill(pygame.color.THECOLORS["blue"])
+        
+            for event in pygame.event.get():  
+                if event.type == QUIT or (event.type == KEYDOWN and (event.key in [K_ESCAPE, K_q])):        
+                    running = False         
+            
+            self.gameMode.update(self.screen);
+            self.gameMode.draw(self.delta);
+            pygame.display.flip()
 
 
 
--- jamtime.py
+++ jamtime.py
@@ -4,10 +4,19 @@ import sys,math
 sys.dont_write_bytecode = 1
 
 from jam.game.JamMode import JamMode
+from jam.framework.Application import Application
+from jam.framework.GameMode import GameMode
+
 
 jam = JamMode()
 jam.init()
+gameMode = GameMode()
+app = Application(gameMode)
+app.init(gameMode) 
 
 while True:
     jam.update(0.1)
     jam.draw(0)
+    app.setMode(gameMode)
+    app.setupWindow()
+    app.runGame()
