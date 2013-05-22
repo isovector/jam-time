cd0c316c9688c06a5f850891f0a1b46f8ab72049
Sandy Maguire <sandy.g.maguire@gmail.com>
2013-05-22 00:22:18 -0700

    Camera focus

--- jam/game/Camera.py
+++ jam/game/Camera.py
@@ -1,5 +1,6 @@
 import math
 import Constants
+from jam.common.Vec2d import Vec2d
 from jam.common.Vec3d import Vec3d
 
 class Camera:
@@ -9,23 +10,31 @@ class Camera:
         self.fov = math.radians(fov)
         self.deadzone = deadzone
         self.farPlane = 50
+        
+        self.pos = Vec3d(0, 0, 0)
         self.focus = Vec3d(0, 0, 0)
+        
         self.offset = (350, 200)
         self.depthMultiplier = Constants.COURT_GFX_DEPTH / Constants.COURT_DEPTH
         self.widthMultiplier = Constants.COURT_GFX_LENGTH / Constants.COURT_LENGTH
         
     def update(self, delta):
-        pass
+        pos = self.toScreen(self.pos)
+        focus = self.toScreen(self.focus)
+        
+        if pos.get_distance(focus) > self.deadzone:
+            self.pos -= (self.pos - self.focus) * delta * 2
+            
         
     def getDepthModifier(self, world):
-        return 1 / ((world.z - self.focus.z) / Constants.COURT_DEPTH + 1.5)
+        return 1 / ((world.z - self.pos.z) / Constants.COURT_DEPTH + 1.5)
 
     def toScreen(self, world):
-        local = world - self.focus
+        local = world - self.pos
         
         depthModifier = self.getDepthModifier(world)
         
         x = local.x * self.widthMultiplier * depthModifier
         y = local.z * self.depthMultiplier + local.y
 
-        return (x + self.offset[0], -y + self.offset[1])
+        return Vec2d(x + self.offset[0], -y + self.offset[1])
--- jam/game/JamMode.py
+++ jam/game/JamMode.py
@@ -10,17 +10,19 @@ class JamMode(GameMode):
     def __init__(self):
         GameMode.__init__(self)
         
-        self.camera = Camera((700,400), (), 10, 150)
+        self.camera = Camera((700,400), (), 10, 100)
         self.court = Court()
         self.entities = []
         
+        self.totalTime = 0
+        
         
     def init(self):
-        self.entities.append(Baller(Vec3d(0, 0, -7)))
+        self.entities.append(Baller(Vec3d(-14, 0, -7)))
         #self.entities.append(Baller(Vec3d(600, 0, 300)))
         #self.entities.append(Baller())
         
-        self.camera.focus = Vec3d(-14, 0, 0)
+        #self.camera.focus = Vec3d(-14, 0, 0)
         
         print("Initialized Jamming")
         
@@ -28,9 +30,13 @@ class JamMode(GameMode):
     def update(self, delta):
         self.camera.update(delta)
         
-        self.camera.focus += Vec3d(0.5, 0, 0) * delta
+        self.totalTime += delta
         
-        self.entities[0].pos += Vec3d(0, 0, 0.5) * delta
+        if 5 < self.totalTime < 10:
+            self.entities[0].pos += Vec3d(5, 0, 2) * delta
+            
+        self.camera.focus = self.entities[0].pos
+        
         #self.entities[1].pos += Vec3d(0, 0, 5) * delta
         #self.entities[2].pos += Vec3d(0, 0, 100.) * delta
         
