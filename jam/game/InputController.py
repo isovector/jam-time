c840ece34005500d30474ee1e06cfef4575d37f6
Justin Franchetto <justin.franchetto@live.ca>
2013-05-27 18:27:26 -0700

    Added an actionQueue, now need to determine how to use the queue for action events

--- jam/game/InputController.py
+++ jam/game/InputController.py
@@ -1,5 +1,7 @@
 from jam.common.Vec3d import Vec3d
 from jam.framework.Application import Application
+import Queue
+
 
 from pygame.locals import *
 nameToKey = {"Up":K_w, "Down":K_s, "Left":K_a, "Right":K_d, "Shoot":K_e, "Pass":K_f, "Turbo":K_t}
@@ -8,10 +10,18 @@ class InputController:
     def __init__(self, owner):
         self.owner = owner
         self.speed = 1.25
+        self.actionQ = Queue.Queue()
+        self.frameCounter = 0
     
     def update(self, delta):
         motion = self.owner.motion
         dir = Vec3d(0, 0, 0)
+        
+        if self.frameCounter == 6:
+            #Collect all items in the queue, find combination and send to onIE
+            self.frameCounter = 0
+        else:
+            self.frameCounter = self.frameCounter + 1
         
         if Application.keymap[nameToKey["Up"]]:
             dir.z = 1
@@ -22,9 +32,11 @@ class InputController:
         if Application.keymap[nameToKey["Right"]]:
             dir.x = 1
         if Application.keymap[nameToKey["Shoot"]]:
-            self.owner.action.shoot()
+            self.actionQ.put("Shoot")
         if Application.keymap[nameToKey["Pass"]]:
-            self.owner.action.passing()
+            self.actionQ.put("Pass")
+        if Application.keymap[nameToKey["Turbo"]]:
+            self.actionQ.put("Turbo")
         
         motion.move(dir * delta * self.speed)
         
