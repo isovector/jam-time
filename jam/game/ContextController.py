a35fcb6612d64e79a40233f271dc6cf6bd69139a
Justin Franchetto <justin.franchetto@live.ca>
2013-06-03 18:52:24 -0700

    Added discussed changes to the InputController and introduced the ContextController
    Added Enum in the common functions for later use

---
+++ jam/game/ContextController.py
@@ -0,0 +1,11 @@
+
+
+class ContextController:
+    def __init__(self):
+        pass 
+    
+    def sendTap(self, key):
+        pass
+    
+    def sendPress(self, key):
+        pass 
--- jam/game/InputController.py
+++ jam/game/InputController.py
@@ -1,44 +1,45 @@
 from jam.common.Vec3d import Vec3d
+from jam.game.ContextController import ContextController
 from jam.framework.Application import Application
-import Queue
-
 
 from pygame.locals import *
-nameToKey = {"Up":K_w, "Down":K_s, "Left":K_a, "Right":K_d, "Shoot":K_e, "Pass":K_f, "Turbo":K_t}
+  
 
 class InputController:
     def __init__(self, owner):
         self.owner = owner
         self.speed = 1.25
-        self.actionQ = Queue.Queue()
+        self.actionBuffer = None
         self.frameCounter = 0
+        self.context = ContextController() 
     
     def update(self, delta):
         motion = self.owner.motion
         dir = Vec3d(0, 0, 0)
         
-        if self.frameCounter == 6:
-            #Collect all items in the queue, find combination and send to onIE
+        if self.frameCounter >= 6:
+            self.context.sendPress(self.actionBuffer)
+            self.actionBuffer = None
             self.frameCounter = 0
-        else:
+        elif self.actionBuffer != None:
             self.frameCounter = self.frameCounter + 1
-        
-        if Application.keymap[nameToKey["Up"]]:
+            
+        if Application.keymap[K_w]:
             dir.z = 1
-        if Application.keymap[nameToKey["Down"]]:
+        if Application.keymap[K_s]:
             dir.z = -1
-        if Application.keymap[nameToKey["Left"]]:
+        if Application.keymap[K_a]:
             dir.x = -1
-        if Application.keymap[nameToKey["Right"]]:
+        if Application.keymap[K_d]:
             dir.x = 1
-        if Application.keymap[nameToKey["Shoot"]]:
-            self.actionQ.put("Shoot")
-        if Application.keymap[nameToKey["Pass"]]:
-            self.actionQ.put("Pass")
-        if Application.keymap[nameToKey["Turbo"]]:
-            self.actionQ.put("Turbo")
-        
+
         motion.move(dir * delta * self.speed)
         
     def onInputEvent(self, event):
-        pass
+        if (self.actionBuffer == None) and (event.type == KEYDOWN):
+            self.actionBuffer = event.key
+        elif (event.type == KEYUP) and(event.key == self.actionBuffer):
+            self.context.sendTap(self.actionBuffer)
+            self.actionBuffer = None
+            
+            
